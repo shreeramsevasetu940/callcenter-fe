49fd6c921f54baa33134c6549cde0dfd707b3671
Akshayvaghasiya772 <akshay.owner@gmail.com>
2025-03-10 17:33:40 +0530

    add

--- pages/_app.js
+++ pages/_app.js
@@ -1,29 +1,4 @@
-// import SideBar from "@/components/SideBar";
-// import ToastComponent from "@/components/ToastComponent";
-// import "@/styles/globals.css";
-// import { SessionProvider } from 'next-auth/react'
-// import Head from "next/head";
-// import { useRouter } from "next/router";
-// export default function App({ Component, pageProps: { session, ...pageProps } }) {
-//   const router=useRouter();
-//   const excludeSideBar =router.pathname.startsWith("/sign-in")||router.pathname.startsWith("/forgot-password")||router.pathname.startsWith("/reset-password")||router.pathname.startsWith("/404")
-//   return (
-//   <>
-//     <SessionProvider session={session}>
-//     <Head>
-// <link rel="icon" href="/images/logo/favicon.png" />
-// </Head>
-// <ToastComponent />
-// {excludeSideBar?<Component {...pageProps} />:
-// <SideBar children={<Component {...pageProps} />}/>}
-//     </SessionProvider>
-//   </>
-//   );
-// }
-
-// import SideBar from "@/components/SideBar";
 import MainBar from "@/components/MainBar";
-// import SideBarComp from "@/components/SideBar";
 import ToastComponent from "@/components/ToastComponent";
 import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
@@ -48,10 +23,8 @@ export default function App({ Component, pageProps: { session, ...pageProps } })
           <link rel="icon" href="/images/logo/favicon.png" />
         </Head>
 
-        {/* Toast Component should be placed here */}
         <ToastComponent />
 
-        {/* Render Component with or without Sidebar based on the condition */}
         {excludeSideBar ? (
           <Component {...pageProps} />
         ) : (
